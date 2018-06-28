a6a015b9b4fcc97abe224fe2e29ecab41357f7a2
unfaiyted <unfaiyted@gmail.com>
2018-06-28 12:44:08 -0500

    first commit

---
+++ src/main/java/com/codeup/qshe/QsheApplication.java
@@ -0,0 +1,12 @@
+package com.codeup.qshe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QsheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QsheApplication.class, args);
+	}
+}
---
+++ src/test/java/com/codeup/qshe/QsheApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codeup.qshe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QsheApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
