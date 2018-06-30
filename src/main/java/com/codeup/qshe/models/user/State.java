4fe986f41b05e0e87ac0e2b4e9fbe7acde9868c4
richardcalata <calatar6@gmail.com>
2018-06-30 12:07:15 -0500

    started states page

--- src/main/java/com/codeup/qshe/models/user/State.java
+++ src/main/java/com/codeup/qshe/models/user/State.java
@@ -1,16 +1,21 @@
-package com.codeup.qshe.models.user;
-
-
-public class State {
-    private String name;
-    private long SGrade;
-    private long PSGrade;
-    private long HGrade;
-    private long PubHGrade;
-    private long EdGrade;
-    private long PubEdGrade;
-    private long EmpGrade;
-    private long PubEmpGrade;
-    private long GGrade;
-    private long PubGGrade;
-}
+//package com.codeup.qshe.models.user;
+//
+//
+//public class State {
+//    private String name;
+//
+//    public State(String name) {
+//        this.name = name;
+//    }
+//
+//    private long SGrade;
+//    private long PSGrade;
+//    private long HGrade;
+//    private long PubHGrade;
+//    private long EdGrade;
+//    private long PubEdGrade;
+//    private long EmpGrade;
+//    private long PubEmpGrade;
+//    private long GGrade;
+//    private long PubGGrade;
+//}
--- src/main/java/com/codeup/qshe/repositories/States.java
+++ src/main/java/com/codeup/qshe/repositories/States.java
@@ -1,19 +1,19 @@
-package com.codeup.qshe.repositories;
-
-import com.codeup.qshe.models.user.State;
-import com.codeup.qshe.models.user.User;
-import com.codeup.qshe.models.user.UserConnection;
-import com.codeup.qshe.models.user.UserProfile;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-
-@Repository
-public interface States extends CrudRepository <State, Long>{
-
-    State findByName(String name);
-
-}
+//package com.codeup.qshe.repositories;
+//
+//import com.codeup.qshe.models.user.State;
+//import com.codeup.qshe.models.user.User;
+//import com.codeup.qshe.models.user.UserConnection;
+//import com.codeup.qshe.models.user.UserProfile;
+//import org.springframework.data.jpa.repository.Modifying;
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.repository.CrudRepository;
+//import org.springframework.stereotype.Repository;
+//import org.springframework.transaction.annotation.Propagation;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//@Repository
+//public interface States extends CrudRepository <State, Long>{
+//
+//    State findByName(String name);
+//
+//}
