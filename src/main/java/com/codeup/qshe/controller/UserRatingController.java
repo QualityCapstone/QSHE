cbbffca548bfe57e1f81489847be9ff35d467053
monicacholico <monicachol@hotmail.com>
2018-07-06 14:56:29 -0500

    Rating User code written, but now working yet. Almost there

--- src/main/java/com/codeup/qshe/controller/UserRatingController.java
+++ src/main/java/com/codeup/qshe/controller/UserRatingController.java
@@ -1,10 +1,14 @@
 package com.codeup.qshe.controller;
 
+import com.codeup.qshe.models.user.User;
 import com.codeup.qshe.models.user.UserRating;
 import com.codeup.qshe.repositories.UserRatingRepository;
 import com.codeup.qshe.repositories.Users;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -19,11 +23,26 @@ public class UserRatingController {
        this.userRatingRepository = userRatingRepository;
    }
 
+    @GetMapping ("/profile/rating")
+    public String showUserRate (Model view) {
+       User usersession = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+       User user = userDao.findById(usersession.getId()).get();
+       if(userRatingRepository.findByUserId(user.getId()) != null){
+           UserRating userRating = userRatingRepository.findByUserId(user.getId());
+           view.addAttribute("user", user);
+           view.addAttribute("userRating", userRating);
+       } else {
+           UserRating userRating = new UserRating();
+           view.addAttribute("user", user);
+           view.addAttribute("userRating", userRating);
+       }
 
+       return "profile/rating";
+    }
 
 
     @PostMapping("/profile/rating")
-    public float newUserRate(
+    public String newUserRate(
             @RequestParam(name = "userId") long userId,
             @RequestParam(name= "stateId") long stateId,
             @RequestParam(name="userRating") float userRate){
@@ -34,8 +53,7 @@ public class UserRatingController {
        }
        UserRating newUserRating = new UserRating(userId, stateId, userRate);
        userRatingRepository.save(newUserRating);
-
-       return Float.parseFloat("redirect:/profile");
+       return "redirect:/profile/rating";
 
     }
 
