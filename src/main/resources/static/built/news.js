3b97742bf3559220c2aea66a7e2050c2c7a6005c
Brian Torluemke <briantorluemke@gmail.com>
2018-07-06 11:10:56 -0500

    news api is getting good requests, began working on implementing

--- src/main/resources/static/built/news.js
+++ src/main/resources/static/built/news.js
@@ -577,9 +577,9 @@ newsapi.v2.topHeadlines({
 // To query /v2/everything
 // You must include at least one q, source, or domain
 newsapi.v2.everything({
-    q: 'bitcoin',
-    sources: 'bbc-news,the-verge',
-    domains: 'bbc.co.uk, techcrunch.com',
+    q: 'texas',
+    // sources: 'bbc-news,the-verge',
+    // domains: 'bbc.co.uk, techcrunch.com',
     from: '2017-12-01',
     to: '2017-12-12',
     language: 'en',
