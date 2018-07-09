2533a82b0b42e1c8648f69b88383e6e7c6d850f5
monicacholico <monicachol@hotmail.com>
2018-07-09 14:56:39 -0500

    refactored statemetricrepository

--- src/main/java/com/codeup/qshe/services/StateUserRatingService.java
+++ src/main/java/com/codeup/qshe/services/StateUserRatingService.java
@@ -1,10 +1,9 @@
 package com.codeup.qshe.services;
 
-import com.codeup.qshe.models.user.Message;
 import com.codeup.qshe.models.user.StateUserRating;
 import com.codeup.qshe.models.user.User;
 import com.codeup.qshe.repositories.Staterepository;
-import com.codeup.qshe.repositories.UserRatingRepository;
+import com.codeup.qshe.repositories.UserRatings;
 import com.codeup.qshe.repositories.Users;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -13,19 +12,19 @@ import java.util.List;
 
 @Service
 public class StateUserRatingService {
-    private UserRatingRepository userRatingRepository;
+    private UserRatings userRatings;
     private Staterepository staterepository;
     private Users userDao;
 
 
-    public StateUserRatingService(UserRatingRepository userRatingRepository, Staterepository staterepository, Users userDao) {
-        this.userRatingRepository = userRatingRepository;
+    public StateUserRatingService(UserRatings userRatings, Staterepository staterepository, Users userDao) {
+        this.userRatings = userRatings;
         this.staterepository = staterepository;
         this.userDao = userDao;
     }
 
     public List<StateUserRating> findAll() {
-        Iterable <StateUserRating> userRatings = userRatingRepository.findAll();
+        Iterable <StateUserRating> stateUserRatingsuserRatings = userRatings.findAll();
         return (List<StateUserRating>) userRatings;
     }
 
@@ -33,23 +32,23 @@ public class StateUserRatingService {
         User sessionUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         User user = userDao.findById(sessionUser.getId()).get();
         userRating.setUser(user);
-        userRatingRepository.save(userRating);
+        userRatings.save(userRating);
         return userRating;
     }
 
     public StateUserRating findOne(long id) {
-        StateUserRating userRating = userRatingRepository.findById(id).get();
+        StateUserRating userRating = userRatings.findById(id).get();
         return userRating;
     }
 
     public StateUserRating deleteUserRating (long id){
-        StateUserRating userRating = userRatingRepository.findById(id).get();
-        userRatingRepository.delete(userRating);
+        StateUserRating userRating = userRatings.findById(id).get();
+        userRatings.delete(userRating);
         return deleteUserRating(id);
     }
 
     public StateUserRating findById (Long id){
-        return userRatingRepository.findById(id).get();
+        return userRatings.findById(id).get();
     }
 
 }
