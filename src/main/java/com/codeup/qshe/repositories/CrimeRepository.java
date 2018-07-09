0f29f29ac5d0ac7c1f3c9385e8f82154407279c0
monicacholico <monicachol@hotmail.com>
2018-07-09 09:45:58 -0500

    test 2

--- src/main/java/com/codeup/qshe/repositories/CrimeRepository.java
+++ src/main/java/com/codeup/qshe/repositories/CrimeRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CrimeRepository extends CrudRepository<StateCrime, Long>{
-    // Nothing is here
+ //second test
 }
