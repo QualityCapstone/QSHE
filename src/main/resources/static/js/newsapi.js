78a70d6f7caa55ca0ab10c5161b543d018e3475d
Brian Torluemke <briantorluemke@gmail.com>
2018-07-09 11:02:50 -0500

    enabled js to show up on page - will implement into state view

--- src/main/resources/static/js/newsapi.js
+++ src/main/resources/static/js/newsapi.js
@@ -1,4 +1,4 @@
-
+    const $ = require('jquery');
     const NewsAPI = require('newsapi');
     const newsapi = new NewsAPI('b8cc911c5a604eadac02a424f0945ae8');
 // To query /v2/top-headlines
@@ -10,46 +10,42 @@
         language: 'en',
         country: 'us'
     }).then(response => {
-        console.log(response);
-        /*
-          {
-            status: "ok",
-            articles: [...]
-          }
-        */
+        // console.log(response);
     });
+
 // To query /v2/everything
 // You must include at least one q, source, or domain
     newsapi.v2.everything({
         q: 'texas',
-        // sources: 'bbc-news,the-verge',
+        sources: 'bbc-news,the-verge,abc-news,cbs-news,cnbc,cnn,fox-news',
         // domains: 'bbc.co.uk, techcrunch.com',
         from: '2018-06-18',
         to: '2018-07-17',
         language: 'en',
-        sortBy: 'relevancy',
-        page: 2
+        sortBy: 'relevancy'
     }).then(response => {
+        console.log(response);
+        console.log(response.articles);
         console.log(response.articles[0]);
+        console.log(response.articles[0].url);
         /*
           {
             status: "ok",
             articles: [...]
           }
         */
+        $('.state').append(
+            "<div class ='card-body'><p>" +
+            response.articles[0].url +
+            "</p></div>"
+        )
     });
+
 // To query sources
 // All options are optional
     newsapi.v2.sources({
-        category: 'technology',
         language: 'en',
         country: 'us'
     }).then(response => {
-        console.log(response);
-        /*
-          {
-            status: "ok",
-            sources: [...]
-          }
-        */
+        // console.log(response);
     });
