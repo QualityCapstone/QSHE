d40b2549a6f3cbe7ea385b6fa921a9ba066970dc
unfaiyted <unfaiyted@gmail.com>
2018-07-12 16:31:25 -0500

    adding more endpoints for data

--- src/main/java/com/codeup/qshe/repositories/StateEducations.java
+++ src/main/java/com/codeup/qshe/repositories/StateEducations.java
@@ -1,9 +1,15 @@
 package com.codeup.qshe.repositories;
 
+import com.codeup.qshe.models.State;
 import com.codeup.qshe.models.StateEducation;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StateEducations extends CrudRepository<StateEducation, Long> {
+
+    List<StateEducation> findAllByState(State state);
+
 }
