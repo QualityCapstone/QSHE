6202712a594b853ab2273acc043440f9fdbceebc
richardcalata <calatar6@gmail.com>
2018-07-12 06:53:27 -0500

    displaying post with correct state id
    Saves new post to db and redirects to correct blog page with new post correct userid and stateid

--- src/main/java/com/codeup/qshe/controller/PostController.java
+++ src/main/java/com/codeup/qshe/controller/PostController.java
@@ -3,6 +3,7 @@ package com.codeup.qshe.controller;
 import com.codeup.qshe.models.State;
 import com.codeup.qshe.models.user.Post;
 import com.codeup.qshe.models.user.User;
+import com.codeup.qshe.repositories.Staterepository;
 import com.codeup.qshe.services.PostService;
 import com.codeup.qshe.services.StateService;
 import com.codeup.qshe.services.user.UserService;
@@ -12,14 +13,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class PostController {
     private final PostService postDao;
     private final UserService userDao;
-    private final StateService stateDao;
+    private final Staterepository stateDao;
 
-    public PostController(PostService postDao, UserService userDao, StateService stateDao){
+
+    public PostController(PostService postDao, UserService userDao, Staterepository stateDao){
         this.postDao = postDao;
         this.userDao = userDao;
         this.stateDao =stateDao;
@@ -40,21 +43,24 @@ public class PostController {
 //        return "posts/all";
 //    }
 
-    @PostMapping("/posts/all")
-    public String createPost (@RequestParam(name = "blogpost") String userInput,
-                              @RequestParam(name = "title") String title,
-                              @RequestParam(name = "state") String state){
+    @PostMapping("/posts/{id}/all")
+    public String createPost (@PathVariable long id, @RequestParam(name = "title") String title,
+                              @RequestParam(name= "blogpost") String blogpost,
+                               Model model){
         System.out.println("hello posts");
         User user = userDao.getLoggedInUser();
-     State thisState=stateDao.findByName(state);
+        State state = stateDao.findById(id).get();
 
         Post post = new Post();
+        model.addAttribute("blogpost", post);
+        post.setBody(blogpost);
         post.setTitle(title);
-        post.setStateId(thisState.getId());
-        post.setBody(userInput);
+        post.setStateId(state.getId());
         post.setUser(user);
+
+
         postDao.save(post);
-        return "redirect:/posts/all";
+        return "redirect:/posts/all/{id}";
     }
 
 }
