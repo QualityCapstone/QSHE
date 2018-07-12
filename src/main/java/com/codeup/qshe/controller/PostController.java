9135671e9a35597cd59c6ce02a0f3ac1a953d747
richardcalata <calatar6@gmail.com>
2018-07-12 17:15:04 -0500

    changed postcontroller

--- src/main/java/com/codeup/qshe/controller/PostController.java
+++ src/main/java/com/codeup/qshe/controller/PostController.java
@@ -32,10 +32,10 @@ public class PostController {
     }
 
     @GetMapping("/posts/all/{id}")
-    private String viewPosts(@PathVariable long id, Model model, @PageableDefault(value=10) Pageable pageable) {
-        List<Post> posts = postDao.getPosts().findAllByStateId(id, pageable);
+    private String viewPosts(@PathVariable long id, Model model, @PageableDefault(value=3) Pageable pageable) {
+//        List<Post> posts = postDao.getPosts().findAllByStateId(id, pageable);
 
-        model.addAttribute("posts", posts);
+        model.addAttribute("posts", postDao.getPosts().findAllByStateId(id, pageable));
 
         return "posts/all";
     }
