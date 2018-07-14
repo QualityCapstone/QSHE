95aaa12c063bc4d0777d7be381588c077fc0c3ac
unfaiyted <unfaiyted@gmail.com>
2018-07-14 15:49:18 -0500

    add changes to post

--- src/main/java/com/codeup/qshe/controller/UserProfileController.java
+++ src/main/java/com/codeup/qshe/controller/UserProfileController.java
@@ -5,6 +5,7 @@ import com.codeup.qshe.models.user.User;
 import com.codeup.qshe.repositories.UserProfiles;
 import com.codeup.qshe.repositories.UserRatings;
 import com.codeup.qshe.services.FlickrService;
+import com.codeup.qshe.services.PostService;
 import com.codeup.qshe.services.StateService;
 import com.codeup.qshe.services.messages.MessagesService;
 import com.flickr4java.flickr.FlickrException;
@@ -27,6 +28,7 @@ public class UserProfileController {
     private StateService stateDao;
     private MessagesService messageDao;
     private UserRatings ratings;
+    private PostService postDao;
 
 
     @Value("${flickr-key}")
@@ -39,7 +41,8 @@ public class UserProfileController {
                                  MessagesService messageDao,
                                  UserService userDao, UserProfiles userProfiles,
                                  StateService stateDao,
-                                 UserRatings ratings
+                                 UserRatings ratings,
+                                 PostService postDao
 ) {
         this.userDetailsLoader = userDetailsLoader;
         this.userProfiles = userProfiles;
@@ -47,6 +50,7 @@ public class UserProfileController {
         this.stateDao = stateDao;
         this.messageDao = messageDao;
         this.ratings = ratings;
+        this.postDao = postDao;
     }
 
     @GetMapping("/users/displayprofile")
@@ -59,8 +63,10 @@ public class UserProfileController {
         State state = stateDao.getStates().findByName(userstate);
 
         FlickrService f = new FlickrService(apiKey, sharedSecret);
-
         model.addAttribute("photo", f.getPhoto(state.getName()));
+
+
+        model.addAttribute("topics", postDao.getTopics().findTop3ByState(state) );
 
         model.addAttribute("overallRating", ratings.avgUserRatingByState(state, user) );
         model.addAttribute("state", state);
