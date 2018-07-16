6d908d8c036ec1d90bac29f6b96a57e031cf5729
unfaiyted <unfaiyted@gmail.com>
2018-07-15 20:18:43 -0500

    adding up avatars

--- src/main/java/com/codeup/qshe/configuration/DataLoader.java
+++ src/main/java/com/codeup/qshe/configuration/DataLoader.java
@@ -568,7 +568,7 @@ public class DataLoader implements ApplicationRunner {
 
             try {
 
-//                ClassPathResource classPathResource = new ClassPathResource("static/img/avatars/" + i +".png");
+                ClassPathResource classPathResource = new ClassPathResource("static/uploads/" + i +".png");
 //                File file= classPathResource.getFile();
 
                 System.out.println( i + " :  moving avatars"); //test
@@ -577,8 +577,10 @@ public class DataLoader implements ApplicationRunner {
                  // String filename = UUID.randomUUID().toString() + ".png";
                  //  String filepath = Paths.get(uploadPath, filename).toString();
 
-            Files.copy(file.toPath(), (new File(uploadPath + "/" + file.getName())).toPath(),
-                    StandardCopyOption.REPLACE_EXISTING);
+                Files.copy(file.toPath(), classPathResource.getFile().toPath());
+
+//            Files.copy(file.toPath(), (new File(uploadPath + "/" + file.getName())).toPath(),
+//                    StandardCopyOption.REPLACE_EXISTING);
 
             data.add(file.getName());
 
