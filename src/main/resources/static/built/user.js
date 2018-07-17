6218b6602681bb1ae6c79f14b2e5fece310883f2
unfaiyted <unfaiyted@gmail.com>
2018-07-17 14:54:03 -0500

    js updates

--- src/main/resources/static/built/user.js
+++ src/main/resources/static/built/user.js
@@ -10437,11 +10437,7 @@ return jQuery;
 
 /***/ }),
 
-<<<<<<< HEAD
-/***/ 187:
-=======
 /***/ 188:
->>>>>>> 074bdb18a76694edec17a622dc16605aebb35ef1
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
