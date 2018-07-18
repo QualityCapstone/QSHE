999f3f3258706b3a9cee0990c3c9f3226b795d6e
Dane Miller <unfaiyted@gmail.com>
2018-07-17 19:13:17 -0500

    fix the ranking values

--- src/main/java/com/codeup/qshe/models/user/StateMetric.java
+++ src/main/java/com/codeup/qshe/models/user/StateMetric.java
@@ -1,6 +1,7 @@
 package com.codeup.qshe.models.user;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table
@@ -39,5 +40,18 @@ public class StateMetric {
     @Override
     public String toString() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StateMetric that = (StateMetric) o;
+        return Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
